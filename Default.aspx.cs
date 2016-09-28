4497785705633d0ff245f80063a17a6d29503400
Jay Mehta <think_jay@hotmail.com>
2016-09-28 16:58:34 -0400

    Authenticate users with their Office 365 accounts
    Demo for authenticating users for your web app using the Office 365
    school or work accounts.

---
+++ Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        String azure365 = "https://login.microsoftonline.com/common/oauth2/authorize?response_type=code&redirect_uri=http%3a%2f%2flocalhost:53493/redirecturl.aspx&client_id=5c04005b-f8f6-4a29-9b80-220740897cde";
+        Response.Redirect(azure365);
+    }
+}
---
+++ redirecturl.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class redirecturl : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        oauth.Text = Request.Params["code"];
+
+        Request.Headers.Add("Authorization", "Bearer " + oauth.Text);
+        Request.Headers.Add("Accept", "application/json;odata.metadata=minimal");
+        
+    }
+}
